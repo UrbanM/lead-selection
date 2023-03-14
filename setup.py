bcf63abeb3c8d52fb207f6d8fc11f4ab168e7d15
UrbanM <urban.marhl@gmail.com>
2023-03-14 15:28:33 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='lead-selection',
+      version='0.0.1',
+      description='Python module for calculating optimal sensor positions in bioelectromagnetism',
+      author='Urban Marhl',
+      author_email='urban.marhl@gmail.com',
+      url='https://urbanmarhl.com/',
+      packages=['lead-selection'],
+     )
